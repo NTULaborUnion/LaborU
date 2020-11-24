d96129ae28dfb3b4aca2f9e7ea6029964b2827a6
軒竹 吳 <jasonwu@webim.com.tw>
2020-11-24 23:53:44 +0800

    move to azure sql

--- LaborU/Startup.cs
+++ LaborU/Startup.cs
@@ -28,8 +28,8 @@ namespace LaborU
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseCosmos(
-                    Configuration.GetConnectionString("DefaultConnection"),"laboru"));
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
